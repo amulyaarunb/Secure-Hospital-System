a178f6c82b54a09f69288404b312195bddcf4409
Harsha Vamsi <harshavamsi096@gmail.com>
2022-03-21 15:06:01 -0700

    Added login fixes

--- app/views.py
+++ app/views.py
@@ -1,6 +1,17 @@
+from django.contrib.auth.models import Group
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
+from django_registration.backends.activation.views import RegistrationView
+from django.contrib.auth.models import Group
+
 
 @login_required
 def index(request):
     return render(request, "home.html")
+
+
+class CustomRegistrationView(RegistrationView):
+    def register(self, form):
+        user =  super().register(form)
+        patient_group = Group.objects.get(name='patient')
+        patient_group.user_set.add(user)
--- shs/urls.py
+++ shs/urls.py
@@ -7,11 +7,12 @@ from django.contrib import admin
 from app import views
 from django_registration.backends.activation.views import RegistrationView
 
+
 urlpatterns = [
     path('', views.index, name="index"),
     path('admin/', admin.site.urls),
     path('accounts/register/',
-         RegistrationView.as_view(success_url='/'),
+         views.CustomRegistrationView.as_view(success_url='/'),
          name='django_registration_register'),
     path('accounts/', include('django_registration.backends.activation.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
