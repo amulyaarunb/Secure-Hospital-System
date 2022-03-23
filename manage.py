070929c0751bd60da6db024b3becc3b29258511a
Harsha Vamsi <harshavamsi096@gmail.com>
2022-03-22 20:11:35 -0700

    adding heroku

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 if __name__ == "__main__":
     if os.environ.get("DJANGO_SETTINGS_MODULE",None) is None:
         os.environ.setdefault("DJANGO_SETTINGS_MODULE",
-                              "shs.settings.local")
+                              "shs.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
