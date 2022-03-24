eb6833a94db4b07c3fe5e9c8642d2ef56ed3bfcd
Harsha Vamsi <harshavamsi096@gmail.com>
2022-03-23 18:34:25 -0700

    fix settings

--- shs/settings.py
+++ shs/settings.py
@@ -25,7 +25,8 @@ print(BASE_DIR)
 SECRET_KEY = '-$qm_tyz+xfkp^x-!90v0l7hy8(!yphb&$trps9a@fo-@6uf_0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+
 
 ALLOWED_HOSTS = []
 
